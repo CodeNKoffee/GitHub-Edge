8fbbe8bd75714b4a3f7beef8d7587be40e4151b9
Hatem Soliman <hatemthedev@gmail.com>
2022-05-21 21:31:39 +0200

    Changed Card layout

--- popup.js
+++ popup.js
@@ -1,7 +1,6 @@
 // Variables
 const searchInput = document.getElementById("search__input");
 const loadingPage = document.getElementById("loading__page");
-const githubApiUrl = "https://api.github.com/users/";
 
 
 // Event Listeners & Functions
@@ -12,6 +11,22 @@ document.addEventListener("keydown", (event) => {
     searchInput.style.display = "none";
     document.body.style.backgroundImage = "none";
     loadingPage.style.display = "block";
+
+
+    let username = searchInput.value;
+    
+    function searchUsers(username) {
+      const githubApiUrl = `https://api.github.com/users/${username}`;
+
+      fetch(githubApiUrl)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+        })
+    }
+
+    searchUsers(username);
+
   } else
     if(event.key == "Enter" && searchInput.value == "") {
       window.open("https://www.github.com");
